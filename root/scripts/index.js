c1ddc8f6b1a3e26a7821aa9d8b53d773f24fb6a4
Juangui <juan-ramirez04@hotmail.com>
2023-06-15 18:36:22 -0500

    update

--- root/scripts/index.js
+++ root/scripts/index.js
@@ -4,8 +4,8 @@ const colorBack = document.body;
 btnDarkMode.addEventListener("click", function () {
   colorBack.classList.toggle("darkMode");
 
-  if (colorBack.classList.)) {
-    btnDarkMode.textContent = "Light Mode";contains("darkMode"
+  if (colorBack.classList.contains("darkMode")) {
+    btnDarkMode.textContent = "Light Mode";
   } else {
     btnDarkMode.textContent = "Dark Mode";
   }
